b4ce127e6ddc7e23696a236d049f72f772af8319
darshinipriyasn <darshinipr2002@gmail.com>
2023-09-25 13:29:37 +0530

    Even Number

---
+++ DemoJavaProject/src/forpatterns/Num2.java
@@ -0,0 +1,16 @@
+package forpatterns;
+
+public class Num2 {
+
+	public static void main(String[] args) {
+		for(int i=160;i>=100;i--)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
